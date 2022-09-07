f35b95d72309b87795e92846e9173fce0e3eae5a
serg-svy <97796536+serg-svy@users.noreply.github.com>
2022-09-07 22:03:19 +0300

    Upload pc

---
+++ js/app.js
@@ -0,0 +1,77 @@
+
+var $slider = $('.slider');
+
+if ($slider.length) {
+    var currentSlide;
+    var slidesCount;
+    var sliderCounter = document.createElement('div');
+    sliderCounter.classList.add('slider__counter');
+
+    var updateSliderCounter = function(slick, currentIndex) {
+        currentSlide = slick.slickCurrentSlide() + 1;
+        slidesCount = slick.slideCount;
+        $(sliderCounter).text(currentSlide + '/' +slidesCount)
+    };
+
+    $slider.on('init', function(event, slick) {
+        $slider.append(sliderCounter);
+        updateSliderCounter(slick);
+    });
+
+    $slider.on('afterChange', function(event, slick, currentSlide) {
+        updateSliderCounter(slick, currentSlide);
+    });
+
+    $slider.slick({
+        prevArrow: $('.js-gallery-prev'),
+        nextArrow: $('.js-gallery-next'),
+    });
+}
+
+
+$('.product_small_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    speed: 300,
+    dots: false,
+    arrows: true,
+    initialSlide: 0,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            }
+        }
+    ]
+})
+
+$('.product_equipment_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll:1,
+    speed: 300,
+    dots: false,
+    arrows: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            }
+        }
+    ]
+})
+
+
+$(document).on('click', '.product_small_slider img', function(){
+    let newimg = $(this).attr('src');
+    $('#big_image').addClass('hidden');
+    $('#big_image').attr('src',newimg);
+    $('#big_image').removeClass('hidden');
+})
+
+
+
